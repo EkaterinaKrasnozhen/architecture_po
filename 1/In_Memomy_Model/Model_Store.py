878b876902e67f9cb1862a845a093dbe5844dbf3
kate <01redcat@mail.ru>
2023-10-11 16:55:52 +0300

    movie

--- 1/In_Memomy_Model/Model_Store.py
+++ 1/In_Memomy_Model/Model_Store.py
@@ -3,12 +3,16 @@ from Model_Elements.Scene import Scene
 from Model_Elements.Flash import Flash
 from Model_Elements.Camera import Camera
 
+
 class Model_Store:
     def __init__(self):
-        self.models = Poligonal_Model()
-        self.scenes = Scene()
-        self.flashes = Flash()
-        self.cameras = Camera()
+        self.models: list[Poligonal_Model] = []
+        self.scenes: list[Scene] = []
+        self.flashes: list[Flash] = []
+        self.cameras: list[Camera] = []
+        if not len(self.models) or not len(self.scenes) or not len(self.flashes) or not len(self.cameras): 
+            # можно в одну строку все?
+            raise f'Должен быть хотя бы 1 объект  в свойствах'
         self._change_observers = []
         
     def get_scene(id_: int):
