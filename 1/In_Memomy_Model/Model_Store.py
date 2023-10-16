0af961557bdd7d4a65bf525e3b693fa1fded5a9f
kate <01redcat@mail.ru>
2023-10-16 10:51:33 +0300

    hw movie

--- 1/In_Memomy_Model/Model_Store.py
+++ 1/In_Memomy_Model/Model_Store.py
@@ -5,7 +5,10 @@ from Model_Elements.Flash import Flash
 from Model_Elements.Camera import Camera
 from IModel_Changer import IModelChanger
 from IModel_Changed_Observer import IModel_Changed_Observer
-
+from Model_Elements.Texture import Texture
+from Stuff.Angle import Angle3D
+from Stuff.Point3D import Point3D
+from Stuff.Color import Color
 
 @implementer(IModelChanger)
 class Model_Store():
@@ -14,9 +17,10 @@ class Model_Store():
         self.scenes: list[Scene] = []
         self.flashes: list[Flash] = []
         self.cameras: list[Camera] = []
-        if not len(self.models) or not len(self.scenes) or not len(self.flashes) or not len(self.cameras):
-            # можно в одну строку все? или лучше разделить?
-            raise f'Должен быть хотя бы 1 объект в свойствах'
+        self.models.append(Poligonal_Model(textures=Texture()))
+        self.scenes.append(Scene(models=list[Poligonal_Model], flashes=list[Flash], cameras=list[Camera]))
+        self.flashes.append(Flash(angle=list[Angle3D], location=list[Point3D], color=list[Color], power=float))
+        self.cameras.append(Camera())
         self._change_observers: list[IModel_Changed_Observer] = []
 
     def get_scene(self, id_: int):
--- 1/Model_Elements/Poligonal_Model.py
+++ 1/Model_Elements/Poligonal_Model.py
@@ -3,7 +3,7 @@ from Texture import Texture
 from Stuff import Point3D
 
 class Poligonal_Model:
-    def __init__(self, textures: Texture): # композиция - poligons, агрегация textures
+    def __init__(self, textures: Texture()): # композиция - poligons, агрегация textures
         self.poligons = list[Poligon]
         self.poligons.append(Poligon(list[Point3D()]))
         self.textures = textures
